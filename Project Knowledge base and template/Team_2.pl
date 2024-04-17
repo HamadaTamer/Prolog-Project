17a4b1ff12a2477d8cabd793ca77c1f7f80db578
Jana Fahmy <163640057+Jojj-F@users.noreply.github.com>
2024-04-17 20:41:36 +0200

    Update Team_2.pl
    append_connection

--- Project Knowledge base and template/Team_2.pl
+++ Project Knowledge base and template/Team_2.pl
@@ -20,8 +20,28 @@ day_slots(Group, Week, Day, S):-
 earliest_slot(Group, Week, Day, H):-
     day_slots(Group, Week, Day , [H|T]).
 
-append_connection(Conn_Source, Conn_Destination, Conn_Duration, Conn_Line, Routes_So_Far, Routes):-
-    append(Routes_So_Far,[route(Conn_Line, Conn_Source, Conn_Destination, Conn_Duration )], Routes ).
+delete_last(List, Result) :-
+        append(Result, [_], List).
+
+        append_connection(Conn_Source, Conn_Destination, Conn_Duration, Conn_Line, Routes_So_Far, Routes):-
+           last(Routes_So_Far,route(Conn_Line1, Conn_Source1, Conn_Destination1, Conn_Duration1 ) ),
+            Conn_Source=Conn_Destination1,
+            Conn_Line =Conn_Line1,
+            D is Conn_Duration +Conn_Duration1,
+            X =Conn_Source1,
+            delete_last(Routes_So_Far, R),
+            append(R,[route(Conn_Line, X, Conn_Destination, D )], Routes ).
+    
+
+
+
+
+            append_connection(Conn_Source, Conn_Destination, Conn_Duration, Conn_Line, Routes_So_Far, Routes):-
+                proper_connection(Conn_Source, Conn_Destination, Conn_Duration, Conn_Line),
+                last(Routes_So_Far,route(Conn_Line1, Conn_Source1, Conn_Destination1, Conn_Duration1 ) ),
+                (Conn_Source\=Conn_Destination1;
+                Conn_Line \=Conn_Line1),
+                append(Routes_So_Far,[route(Conn_Line, Conn_Source, Conn_Destination, Conn_Duration )], Routes ).
 
 proper_connection(Station_A, Station_B, Duration, Line):-
     (connection(Station_A, Station_B, Duration, Line); connection(Station_B, Station_A, Duration, Line)),
